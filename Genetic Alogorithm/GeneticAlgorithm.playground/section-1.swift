8f072f308f69f47f4fafcf33f17246cb93350bd8
Muhammad Raza Master <mrazam110@gmail.com>
2015-05-02 23:06:39 -0700

    Tour Manager Class
    Tour Manager Class

--- Genetic Alogorithm/GeneticAlgorithm.playground/section-1.swift
+++ Genetic Alogorithm/GeneticAlgorithm.playground/section-1.swift
@@ -31,4 +31,43 @@ public class City {
     
 }
 
+
+public class TourManager {
+    
+    // Holds our cities
+    class var destinationCities:[City] {
+        get{
+            return self.destinationCities
+        }set{
+            self.destinationCities = newValue
+        }
+    }
+    
+    // Adds a destination city
+    public class func addCity(city: City) {
+        destinationCities.append(city)
+    }
+    
+    // Get a city
+    public class func getCity(index: Int) -> City {
+        return destinationCities[index]
+    }
+    
+    // Get the number of destination cities
+    public class func numberOfCities() -> Int {
+        return destinationCities.count
+    }
+    
+}
+
+public class Tour {
+    
+    // Holds our tour of cities
+    private(set) var tour = [City]()
+    
+    // Cache
+    
+    
+}
+
 var c = City(x: 20, y: 20)
